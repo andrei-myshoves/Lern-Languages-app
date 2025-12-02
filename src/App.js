ba2b5b592e9f46bb329682ca92c58702fa9f8d01
andrei-mushovets <andreimyshoves89@gmail.com>
2025-12-02 18:05:18 +0100

    add basic arcitecture and component Header with styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import AppRouter from './routes/AppRouter.jsx'
+import Header from './components/Header/Header.jsx'
 
 function App() {
-    return <AppRouter />
+    return (
+        <div className="App">
+            <Header />
+
+            {/* остальные компоненты */}
+        </div>
+    )
 }
 
 export default App
