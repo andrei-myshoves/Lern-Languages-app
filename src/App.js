54b8c136c905589cc92256ae95cdfd71e53786ef
andrei-mushovets <andreimyshoves89@gmail.com>
2025-12-05 17:17:26 +0100

    delete unnessary files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header.jsx'
 
 function App() {
     return (
-        <div className="App">
+        <div>
             <Header />
             <AppRouter />
         </div>
